8d679ff543559ddac9cee1ff2e9c9266a7ede539
L.Carquet <laetiticarquet@gmail.com>
2016-07-22 16:40:13 +0200

    create a Shake shop class to make a list of milkshakes and the overall total

--- shake_shack.rb
+++ shake_shack.rb
@@ -1,11 +1,14 @@
 class MilkShake
+	attr_reader :base_price, :ingredients
 	def initialize
 		@base_price = 3
-		@ingredients = []
+		@ingredients = []	
 	end
+
 	def add_ingredient(ingredient)
 		@ingredients.push(ingredient)
 	end
+
 	def price_of_milkshake
 		total_price_of_milkshake = @base_price
 		@ingredients.each do |ingredient|
@@ -15,11 +18,34 @@ class MilkShake
 	end
 end
 
+
+
 class Ingredient
 	attr_reader :name, :price 
 	def initialize(name, price)
 		@name = name
 		@price = price
+	end
+end
+
+#create a ShakeShop class
+
+class ShakeShop
+#create an empty list where we can add milkshake
+	def initialize
+		@list = []
+	end
+#create a method to add milkshake on that list
+	def add_my_milkshake(milkshake)
+		@list.push(milkshake)
+	end
+#total to pay is included in the list of shakes.take one element of 
+	def check_out
+		total_to_pay = 0
+		@list.each do |milkshake|  
+			total_to_pay += milkshake.price_of_milkshake
+		end
+		total_to_pay
 	end
 end
 
@@ -30,3 +56,29 @@ nizars_milkshake.add_ingredient(banana)
 nizars_milkshake.add_ingredient(chocolate_chips)
 
 puts nizars_milkshake.price_of_milkshake
+
+detox_milkshake = MilkShake.new
+green_stuff = Ingredient.new("green stuff", 2)
+air = Ingredient.new("air", 3)
+detox_milkshake.add_ingredient(green_stuff)
+detox_milkshake.add_ingredient(air)
+
+puts detox_milkshake.price_of_milkshake
+
+naughty_milkshake = MilkShake.new
+strawberries = Ingredient.new("strawberries", 4)
+whipcream = Ingredient.new("whipcream", 3)
+naughty_milkshake.add_ingredient(banana)
+naughty_milkshake.add_ingredient(strawberries)
+naughty_milkshake.add_ingredient(whipcream)
+
+puts naughty_milkshake.price_of_milkshake
+
+
+laetitia = ShakeShop.new
+laetitia.add_my_milkshake(detox_milkshake)
+laetitia.add_my_milkshake(nizars_milkshake)
+laetitia.add_my_milkshake(naughty_milkshake)
+puts "Total to pay is #{laetitia.check_out}"
+
+
